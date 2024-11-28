444ede3b7efb5dc92ace558098eab63da77aef80
utkarshg1 <gaikwadujg@gmail.com>
2024-11-28 23:13:11 +0530

    updated investment calculation

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+  @Output() calculate = new EventEmitter<{
+    initialInvestment: number;
+    annualInvestment: number;
+    expectedReturn: number;
+    duration: number;
+  }>();
+  enteredInitialInvestment = "0";
+  enteredAnnualInvestment = "0";
+  enteredExpectedReturn = "5";
+  enteredDuration = "10";
 
+  onSubmit() {
+    console.log("Submitted!");
+    this.calculate.emit({
+      initialInvestment: +this.enteredInitialInvestment,
+      annualInvestment: +this.enteredAnnualInvestment,
+      expectedReturn: +this.enteredExpectedReturn,
+      duration: +this.enteredDuration,
+    });
+  }
 }
